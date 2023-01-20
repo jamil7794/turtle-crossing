926b9c02be2f473fa5c3c4c8d5421306e7970c17
yourusername <jamil.jalal94@gmail.com>
2023-01-20 14:51:51 -0500

    make cars and detect collisions

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
@@ -16,3 +19,12 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
